a1d9119d770c19e8cea92ce7b82609f382f8dc6a
peterzhou <zhoushuning@gmail.com>
2018-11-03 18:00:23 -0700

    2018-11-02

--- project/test_project/test_project/scheduler.py
+++ project/test_project/test_project/scheduler.py
@@ -34,7 +34,7 @@ while True:
         # This is for avoiding running multiple crawlers for the same day.
         running = False
 
-    if hour == 17 and minute < 5:
+    if hour == 22 and minute < 30:
         # schdeule a run around 17:00.
         if running == False:            
             if isWeekday():
